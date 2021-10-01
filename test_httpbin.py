2169ee7394a2e523476b4fc7f9689d756c2ffecb
Lida <lgliducik@gmail.com>
2021-10-01 09:58:40 +0300

    first commit

---
+++ test_httpbin.py
@@ -0,0 +1,41 @@
+import pytest
+import logging
+import requests
+
+
+log = logging.getLogger('urllib3')  # works
+log.setLevel(logging.DEBUG)  # needed
+fh = logging.FileHandler("test.log")
+log.addHandler(fh)
+
+
+def test_header_ok():
+    url_headers = "http://httpbin.org/headers"
+    r = requests.get(url_headers, headers={'One': 'true'})
+    assert "OK" == r.reason
+    assert r.json()['headers']['One'] == 'true'
+    #assert r.header['one'] == 'true'
+
+
+def test_header_incorrect_request():
+    with pytest.raises(requests.exceptions.ConnectionError):
+        res = requests.get(
+            "http://httpbin.org/headers",
+            headers={'Content-Length': '200'}
+        )
+
+
+def test_status_multiples_code():
+    codes = [200, 300, 400, 500]
+    url_all_code = "http://httpbin.org/status/" + ",".join(map(str, codes))
+    
+    r = requests.get(url_all_code)
+    assert r.status_code in codes
+
+
+@pytest.mark.parametrize("request_type", [requests.get, requests.post, requests.put, requests.delete, requests.patch])
+@pytest.mark.parametrize("code", [200, 300, 400, 500])
+def test_status_code(code, request_type):
+    url_all_code = "http://httpbin.org/status/"+str(code)
+    r = request_type(url_all_code)
+    assert r.status_code == code
